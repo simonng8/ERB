be87ba93bdf986fdddddb3b9d426aad9daa87bf0
simon <thng1783@gmail.com>
2025-05-29 11:51:39 +0800

    step 5: install models

--- bcre/settings.py
+++ bcre/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'bcre.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bcredb', 
+        'USER' : 'postgres',
+        'PASSWORD' : os.getenv('DB_PASSWORD'),
+        'HOST' : 'localhost',
+        'PORT' : 5432
     }
 }
 
@@ -141,3 +145,6 @@ INTERNAL_IPS = [
     "127.0.0.1",
     
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
